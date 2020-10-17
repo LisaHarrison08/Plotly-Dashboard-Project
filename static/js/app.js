a1df3449b5f36fcf2a09a2942d4120eb51388c07
Lisa Harrison <lmharrison0810@gmail.com>
2020-10-16 23:57:49 -0400

    Demographic info displayed in dashboard

--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,35 @@ function init() {
           .text(value)
           .attr("value", value);
       });
+
+      demoInfo(names[0]);
     });
   }
   //Names added to the dropdown
   init();
+
+  d3.selectAll("#selDataset").on("change", dashFunctions);
+
+  // Function to initiate plots
+  function dashFunctions() {
+    var testSubject = d3.select("#selDataset").node().value;
+    demoInfo(testSubject);
+  }
+
+//Function to build demographic info
+function demoInfo(testSubject) {
+  // console.log(testSubject);
+  var mdata = data.metadata.filter((value) => value.id == testSubject);
+  var dInfo = d3.select(".panel-body");
+  dInfo.html("");
+  dInfo.append("p").text(`id: ${mdata[0].id}`);
+  dInfo.append("p").text(`Ethnicity: ${mdata[0].ethnicity}`);
+  dInfo.append("p").text(`Gender: ${mdata[0].gender}`);
+  dInfo.append("p").text(`Age: ${mdata[0].age}`);
+  dInfo.append("p").text(`Location: ${mdata[0].location}`);
+  dInfo.append("p").text(`bbtype: ${mdata[0].bbtype}`);
+  dInfo.append("p").text(`wfreq: ${mdata[0].wfreq}`);
+}
+
+//Function to build a Horizontal Bar Chart using sample data
+// Use otu_labels as the hovertext for the chart
